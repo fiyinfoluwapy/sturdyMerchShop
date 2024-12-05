7a1a3f3bd74a7e40795a49019007e28fac3d1391
Temidire Damian <vladimirwayne9@gmail.com>
2024-12-05 19:17:38 +0000

    bestseller tweaks

--- src/main.js
+++ src/main.js
@@ -18,18 +18,20 @@ document.addEventListener('DOMContentLoaded', () => {
     animatedElements.forEach(el => observer.observe(el));
   }
 });
-    // Mobile Navigation Toggle
-    document.getElementById('menu-button').addEventListener('click', () => {
-      const mobileMenu = document.getElementById('mobile-menu');
-      mobileMenu.classList.add('open');
-      mobileMenu.style.display = 'block'; // Ensure it's visible
-  });
+// Mobile Navigation Toggle
+document.getElementById('menu-button').addEventListener('click', () => {
+  const mobileMenu = document.getElementById('mobile-menu');
+  mobileMenu.classList.add('open');
+  mobileMenu.style.display = 'block'; // Ensure it's visible
+});
 
-  document.getElementById('close-menu').addEventListener('click', () => {
-      const mobileMenu = document.getElementById('mobile-menu');
-      mobileMenu.classList.remove('open');
-      setTimeout(() => {
-          mobileMenu.style.display = 'none'; // Hide after transition
-      }, 300); 
-  });
+document.getElementById('close-menu').addEventListener('click', () => {
+  const mobileMenu = document.getElementById('mobile-menu');
+  mobileMenu.classList.remove('open');
+  setTimeout(() => {
+    mobileMenu.style.display = 'none'; // Hide after transition
+  }, 300);
+});
+
+
 
