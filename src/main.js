e5e51d611545cb20e4b019be32ce1ab27a301bc3
Temidire Damian <vladimirwayne9@gmail.com>
2024-12-06 06:26:40 +0000

    swiper responsiveness

--- src/main.js
+++ src/main.js
@@ -35,3 +35,33 @@ document.getElementById('close-menu').addEventListener('click', () => {
 
 
 
+const swiper = new swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+  breakpoints: {
+      480: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+      },
+      640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+      },
+      768: {
+          slidesPerView: 2,
+          spaceBetween: 25,
+      },
+      1024: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+      },
+  },
+});
